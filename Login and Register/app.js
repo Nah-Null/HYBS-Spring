80fa4f97f642e1cfa8cd00d0c3f8d9c54156ce71
Nah-Null <pgthman@gmail.com>
2025-02-20 14:23:56 +0700

    update test 1

--- Login and Register/app.js
+++ Login and Register/app.js
@@ -164,23 +164,25 @@ async function open_member() {
 
     window.location.href = "https://nah-null.github.io/HYBS-Spring/Login%20and%20Register/mumber.html";
 
-    // document.getElementById('login-email').value = '';
-    // document.getElementById('login-password').value = '';
+
+
+    document.getElementById('login-email').value = '';
+    document.getElementById('login-password').value = '';
     
-    // alert(localStorage.getItem('email'));
+    alert(localStorage.getItem('email'));
 
-    // try {
-    //     const response = await fetch(`http://localhost:4000/users/getByEmail?email=${localStorage.getItem('email')}`);
-    //     if (!response.ok) throw new Error('Network response was not ok');
-    //     const data = await response.json();
+    try {
+        const response = await fetch(`http://localhost:4000/users/getByEmail?email=${localStorage.getItem('email')}`);
+        if (!response.ok) throw new Error('Network response was not ok');
+        const data = await response.json();
 
-    //     document.getElementById('name').innerText = data.name;
-    //     document.getElementById('email').innerText = data.email;
-    //     alert(data.name);
-    //     alert(data.email);
-    // } catch (error) {
-    //     console.error("Error fetching user data:", error);
-    // }
+        document.getElementById('name').innerText = data.name;
+        document.getElementById('email').innerText = data.email;
+        alert(data.name);
+        alert(data.email);
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+    }
 }
 
 // ฟังก์ชันสำหรับออกจากระบบ
